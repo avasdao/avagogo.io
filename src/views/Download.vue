e861f154abf7d65fc55d434d1b1ef6a15f0d3568
Shomari <nyusternie@sdot.io>
2022-02-19 14:04:38 -0700

    Update download page.

--- src/views/Download.vue
+++ src/views/Download.vue
@@ -1,6 +1,22 @@
 <template>
     <main class="max-w-5xl mx-auto">
         <h1 class="text-3xl my-10">Download Our Mobile App</h1>
+
+        <div class="mt-5 mb-10 p-5 bg-yellow-200 border-2 border-yellow-400 rounded-xl">
+            <h3 class="text-2xl font-medium">
+                Would you like to try our iOS "Beta" in TestFlight?
+            </h3>
+
+            <p class="mt-3 mb-10">
+                Please keep in mind that this is an early preview of the Ava GoGo mobile app.
+                <br />Your mileage may vary.
+            </p>
+
+            <!-- Moralis + Avalanche Hackathon v22.2.18 -->
+            <a class="p-2 bg-blue-300 border-2 border-blue-500 text-2xl rounded-xl hover:bg-blue-500 hover:border-blue-700 hover:text-gray-50" href="https://testflight.apple.com/join/ONgw6wrz" target="_blank">
+                Click here to download (v22.2.18)
+            </a>
+        </div>
     </main>
 </template>
 
